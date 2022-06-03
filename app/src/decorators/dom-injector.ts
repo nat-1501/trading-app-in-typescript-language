0ad0e52a736ebc96e25ba319e56bb1cc22dce2ab
Nat-1501 <natalisoares90@gmail.com>
2022-06-03 12:10:58 -0300

    Criando o próprio DOM Injector

---
+++ app/src/decorators/dom-injector.ts
@@ -0,0 +1,7 @@
+export function domInjector(seletor: string) {
+    return function(target: any, propertyKey:string) {
+        
+    }
+}
+
+
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -15,8 +15,6 @@ export abstract class View<T> {
 
     }
 
-    @logarTempoDeExecucao(true)
-    @inspect()
 
     public update(model: T): void {
         let template = this.template(model);
